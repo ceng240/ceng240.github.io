fa0ea88bbb4b83ae08bc3d7c1d2d8596e84f4c93
Onur Tolga Sehitoglu <onur@dhcp-29.ceng.metu.edu.tr>
2020-11-05 18:24:35 +0300

    Browser based editor for IDE

---
+++ pyeditor/src/version_info.js
@@ -0,0 +1,35 @@
+__BRYTHON__.implementation = [3, 9, 0, 'final', 0]
+__BRYTHON__.__MAGIC__ = "3.9.0"
+__BRYTHON__.version_info = [3, 9, 0, 'final', 0]
+__BRYTHON__.compiled_date = "2020-10-06 15:00:12.792958"
+__BRYTHON__.timestamp = 1601989212792
+__BRYTHON__.builtin_module_names = ["_aio",
+    "_ajax",
+    "_base64",
+    "_binascii",
+    "_io_classes",
+    "_json",
+    "_jsre",
+    "_locale",
+    "_multiprocessing",
+    "_posixsubprocess",
+    "_profile",
+    "_sre_utils",
+    "_string",
+    "_strptime",
+    "_svg",
+    "_warnings",
+    "_webcomponent",
+    "_webworker",
+    "_zlib_utils",
+    "array",
+    "builtins",
+    "dis",
+    "hashlib",
+    "long_int",
+    "marshal",
+    "math",
+    "modulefinder",
+    "posix",
+    "random",
+    "unicodedata"]
